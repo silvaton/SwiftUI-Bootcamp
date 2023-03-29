0e5c362971a944faacff97ee1c1c2bafc539440f
Ton Silva <silvatonsp@gmail.com>
2023-03-29 17:34:57 +0200

    Inits and Enums

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/InitializerBootcamp.swift
@@ -0,0 +1,57 @@
+//
+//  InitializerBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Ton Silva on 29/3/23.
+//
+
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = .red
+        } else {
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
+    }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .underline()
+            
+            Text("\(title)")
+                .font(.headline)
+                .foregroundColor(Color.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .cornerRadius(10)
+    }
+}
+
+struct InitializerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack {
+            InitializerBootcamp(count: 100, fruit: .apple)
+            InitializerBootcamp(count: 50, fruit: .orange)
+        }
+    }
+}
