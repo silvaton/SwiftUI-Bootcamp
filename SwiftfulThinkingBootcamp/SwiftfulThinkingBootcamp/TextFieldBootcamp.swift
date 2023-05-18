8200bcef0cf05084bf27eac12f305e3ee13df514
silvaton <tdasilva@ext.laligatech.com>
2023-05-18 11:06:36 +0200

    TextField in SwiftUI

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextFieldBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  TextFieldBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Ton Silva on 18/5/23.
+//
+
+import SwiftUI
+
+struct TextFieldBootcamp: View {
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Type something here ...", text: $textFieldText)
+                    //.textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(10)
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.red)
+                    .font(.headline)
+                
+                Button {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                } label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ? Color.blue : Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextField Bootcamp!")
+        }
+    }
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count >= 3 {
+            return true
+        }
+        return false
+    }
+    
+    func saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+struct TextFieldBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldBootcamp()
+    }
+}
