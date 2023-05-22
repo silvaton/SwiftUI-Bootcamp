f90efb235ab0725375caaed80b139a9d26141990
silvaton <tdasilva@ext.laligatech.com>
2023-05-22 17:26:25 +0200

    TextEditor in SwiftUI

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextEditorBootcamp.swift
@@ -0,0 +1,50 @@
+//
+//  TextEditorBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Ton Silva on 22/5/23.
+//
+
+import SwiftUI
+
+struct TextEditorBootcamp: View {
+    
+    @State var textEditorText: String = "This is the starting text"
+    @State var savedText: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $textEditorText)
+                    .frame(height: 250)
+                    .colorMultiply(.gray)
+                    .cornerRadius(10)
+                    .bold()
+
+                Button {
+                    savedText = textEditorText
+                } label: {
+                    Text("Save".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+                
+                Text(savedText)
+
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextEditor Bootcamp!")
+        }
+    }
+}
+
+struct TextEditorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorBootcamp()
+    }
+}
